0f86ed0cfa5be18e7e1a47528b8e087d3e4d1644
Sarwat Jahan <159925204+SarwatJahan22@users.noreply.github.com>
2024-03-16 20:40:44 +0500

    Add files via upload
    asignment about 'cases' which were taught in clas.

---
+++ main.ts
@@ -0,0 +1,66 @@
+//  camelCase
+//  snake_case
+//  PascalCase
+//let userName = 'Sarwat' // e.g. camelCase
+//let user_name = 'Zara' // e.g. snake_case
+//let UserName = 'Kashaf' // e.g. PascalCase
+
+//let isUserExist = true;
+//let is_user_exist = false;
+//let IsUserExist = 1;
+
+// = is assignment operator 
+
+//console.log(userName, user_name, UserName)
+//console.log(isUserExist, is_user_exist, IsUserExist)
+
+//let _a = ('hello');
+//console.log(_a)
+
+//let $abida = ('raheel') 
+//let message = ('how are you')
+
+//console.log ($abida,message)
+
+//let anyType;
+
+// Arrays => [] 0, ...
+
+//let students =['Sarwat', 'Zara', 'Kashaf']
+
+///console.log(students)
+//  let firstStudent = students[0]
+
+//  console.log (firstStudent)
+//  console.log(students[1])
+//  console.log(students[2])
+
+//  console.log(students[1], students[2])
+
+
+
+ //let students: (string | number | boolean )[]=['Sarwat', 'Zara', 'Kashaf', 1, 10, true]
+
+
+//console.log(students)
+
+let student: (number) []= [1,2,3]
+console.log(student) 
+
+let boolean: (boolean) []= [true,false]
+
+console.log(boolean)
+
+
+// changing value from one to another
+
+let userName: string = "ALI"
+userName = "daniyal"
+
+let fruits = ["apple", "mango", "banana"]
+
+fruits[1] = "grapes"
+
+console.log(fruits)
+
+
